9670773ecd1793a7b56e9ebfaecdf6fc9888c61c
sarahysh12 <saraysh22@gmail.com>
2020-04-08 17:47:47 -0700

    add search bar

--- src/containers/SubReddits/SubReddits.js
+++ src/containers/SubReddits/SubReddits.js
@@ -23,14 +23,36 @@ class SubReddits extends Component {
         this.setState({selectedSubRedditUrl: url});
     }
 
+    //TODO fix the issue when you empty the seacrh bar
+    //TODO add no result found
+    //TODO what is autocomplete?
+    onSearchHandler = (evt) => {
+        const keyword = evt.target.value;
+        var regex = new RegExp(keyword);
+        let searchResult = [];
+        this.state.subReddits.some(sub => {
+            if(sub.data.title.toLowerCase().match(regex)){
+                searchResult.push(sub)
+            }
+        });
+        if (searchResult.length > 0){
+            this.setState({subReddits: searchResult})
+        }
+    }
+
     render() {
         const subs = this.state.subReddits.map(sub => {
             return <SubReddit title={sub.data.title}
                 key = {sub.data.id}
                 clicked={() => this.subRedditSelectedHandler(sub.data.url)}/>
         });
+
+
         return (
             <div>
+                <section className="SearchBox" >
+                    <input type="text" placeholder="Search" onChange={this.onSearchHandler}/>
+                </section>
             <section className="SubReddits">
                 {subs}
             </section>
